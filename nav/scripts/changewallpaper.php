6a7c831c3a3ad93f63c351102f2f668e09b3b268
shaoziyang <shaoziyang@126.com>
2023-11-14 23:23:47 +0800

    Update changewallpaper.php

--- nav/scripts/changewallpaper.php
+++ nav/scripts/changewallpaper.php
@@ -5,7 +5,8 @@ $file_last = 'last.txt';
 
 if (file_exists($file_last))
 {
-    unlink($file_last);    
+    unlink($file_last);   
+    header('location:'.getenv("HTTP_REFERER"));
     header("Refresh:0");
 }
 
