55cb6dc0d881cb0e9fa0532bbdbd7b840cc491a5
shaoziyang <shaoziyang@126.com>
2023-11-14 23:24:59 +0800

    主界面增加更换背景链接

--- nav/template.inc.php
+++ nav/template.inc.php
@@ -298,7 +298,7 @@
 		<div class="divider"></div>
 		<div class="row">
 			<div class="col m5 offset-m1 hide-on-med-and-down">
-				<p class="left-align"><small><?php include 'scripts/showcounter.php'; ?></small></p>
+				<p class="left-align"><small><?php include 'scripts/showcounter.php'; ?>&emsp;<a href="scripts/changewallpaper.php">更换背景</a></small></p>
 			</div><!-- /col -->
 			<div class="col m5 hide-on-med-and-down">
 				<p class="right-align"><small><a href="https://github.com/shaoziyang/webnav" target="_blank"><b>随心远航</b>网址导航</a><?php if($APP->DEBUG){echo " ".$APP->VERSION;} if(Session::getInstance()->isAuthenticated()){echo " - <a href=\"".$DOC->URL."?exit\">".$TXT->Logout."</a>";} ?></small></p>
