4aabe1c23dda7bd768a5c4df9286d535557c0b5c
shaoziyang <shaoziyang@126.com>
2023-11-20 00:10:09 +0800

    增加搜索统计

--- nav/scripts/linkcount.js
+++ nav/scripts/linkcount.js
@@ -1,12 +1,18 @@
-var links = document.getElementsByTagName('a');
+var link1 = document.getElementsByTagName('a');
+var link2 = document.getElementsByClassName('sxyh_search');
 
+for (var i = 0; i < link1.length; i++) {  
 
-for (var i = 0; i < links.length; i++) {  
-
-  links[i].addEventListener('click', function(event) {  
+  link1[i].addEventListener('click', function(event) {  
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'scripts/counter.php', true);
     xhr.send(null);})
-
 }
 
+for (var i = 0; i < link2.length; i++) {  
+
+  link2[i].addEventListener('click', function(event) {  
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'scripts/counter.php', true);
+    xhr.send(null);})
+}
