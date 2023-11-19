4c0836c06f092d2dc6a785cc565e762c191202f2
shaoziyang <shaoziyang@126.com>
2023-11-19 11:23:54 +0800

    增加不缓存文件设置

--- nav/template.inc.php
+++ nav/template.inc.php
@@ -14,7 +14,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php include "scripts/wallpaper.php"; ?>
+	<meta http-equiv="pragma" content="no-store" />
+	<meta http-equiv="content-type" content="no-store, must-revalidate" />
+	<meta http-equiv="expires" content="Wed, 26 Feb 1997 08:21:57 GMT"/>
+	<?php include "scripts/wallpaper.php"; ?>
 	<link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>helpers/materialize-1.0.0/css/materialize.min.css" media="screen,projection"/>
 	<link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>helpers/material-icons-1.13.6/css/material-icons.css" media="screen,projection"/>
 	<link type="text/css" rel="stylesheet" href="<?= $APP->PATH ?>helpers/font-awesome-4.7.0/css/font-awesome.min.css" media="screen,projection"/>
