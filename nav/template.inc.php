d519f6ea7a7d434396ac050c7a78aba15a5aaba9
shaoziyang <shaoziyang@126.com>
2023-11-20 14:01:20 +0800

    修改背景图库链接

--- nav/template.inc.php
+++ nav/template.inc.php
@@ -300,13 +300,13 @@
 		<div class="divider"></div>
 		<div class="row">
 			<div class="col m5 offset-m1 hide-on-med-and-down">
-				<p class="left-align"><small><?php include 'scripts/showcounter.php'; ?>&emsp;<script>document.write('<a href="<?= $APP->PATH ?>scripts/changewallpaper.php" title='+back_image+'>更换背景</a>(<a href="<?= $APP->PATH ?>scripts/showimagefile.php" title="显示背景图像列表">'+back_image+'</a>)');</script></small></p>
+				<p class="left-align"><small><?php include 'scripts/showcounter.php'; ?>&emsp;<script>document.write('<a href="<?= $APP->PATH ?>scripts/changewallpaper.php" title='+back_image+'>更换背景</a>&emsp;<a href="<?= $APP->PATH ?>scripts/showimagefile.php" title="显示背景图像列表">背景图库</a>');</script></small></p>
 			</div><!-- /col -->
 			<div class="col m5 hide-on-med-and-down">
 				<p class="right-align"><small><a href="https://github.com/shaoziyang/webnav" target="_blank"><b>⛵随心远航</b>网址导航</a><?php if($APP->DEBUG){echo " ".$APP->VERSION;} if(Session::getInstance()->isAuthenticated()){echo " - <a href=\"".$DOC->URL."?exit\">".$TXT->Logout."</a>";} ?></small></p>
 			</div><!-- /col -->
 			<div class="col s12 hide-on-large-only">
-				<p class="center-align"><small><?php include 'scripts/showcounter.php'; ?><script>document.write('<br><a href="<?= $APP->PATH ?>scripts/changewallpaper.php" title='+back_image+'>更换背景</a>(<a href="<?= $APP->PATH ?>scripts/showimagefile.php" title="显示背景图像列表">'+back_image+'</a>)');</script></small></p>
+				<p class="center-align"><small><?php include 'scripts/showcounter.php'; ?><script>document.write('<br><a href="<?= $APP->PATH ?>scripts/changewallpaper.php" title='+back_image+'>更换背景</a>&emsp;<a href="<?= $APP->PATH ?>scripts/showimagefile.php" title="显示背景图像列表">背景图库</a>');</script></small></p>
 				<p class="center-align"><small><b><?= $APP->OWNER ?></b><br><?= $APP->NOTICE ?></small></p>
 				<p class="center-align"><small><a href="https://github.com/shaoziyang/webnav" target="_blank"><b>⛵随心远航</b>网址导航</a><?php if($APP->DEBUG){echo " ".$APP->VERSION;} if(Session::getInstance()->isAuthenticated()){echo " - <a href=\"".$DOC->URL."?exit\">".$TXT->Logout."</a>";} ?></small></p>
 			</div><!-- /col -->
