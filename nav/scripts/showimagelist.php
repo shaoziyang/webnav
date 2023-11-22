0217eab24f96957d1a216de9b7a86d4648a39692
shaoziyang <shaoziyang@126.com>
2023-11-22 16:24:14 +0800

    优化显示系统背景图库功能

--- nav/scripts/showimagelist.php
+++ nav/scripts/showimagelist.php
@@ -44,22 +44,23 @@ if (file_exists($file_cfg))
 
 echo '<a href="../">返回导航</a>&emsp;<a href="bingwallpaper.php">必应每日一图</a><br><br>';
 
-echo '<form action="setwallpaperinterval.php" name="formu"><p>背景自动更新时间 '.$DT.' 秒（0代表禁止自动更新）。<input type="text" name="time" value="'.$DT.'" maxlength="6" size="8" style="text-align:right;"> 秒 <input type="submit" style="border:0px;width:120px" class="sxyhButton_03" value="设置新时间" />&emsp;<a class="sxyhButton_03" style="border:0px;width:120px;background-color:#2875ed;" href="changewallpaper.php" onclick="history.go(0)">立即更新背景</a></p></form>';
+echo '<form action="setwallpaperinterval.php" name="formu"><p>背景自动更新时间 '.$DT.' 秒（0代表禁止自动更新）。<input type="text" name="time" value="'.$DT.'" maxlength="6" size="8" style="text-align:right;"> 秒 <input type="submit" style="border:0px;width:120px" class="sxyhButton_03" value="设置新时间" />&emsp;<a class="sxyhButton_03" style="border:0px;width:120px;background-color:#A000FF;" href="changewallpaper.php" onclick="history.go(0)">立即更新背景</a></p></form>';
 
 // get image file list
 $image_list = array_merge(glob('../images/*.png'), glob('../images/*.jpg'), glob('../images/*.webp'));
 
-$fl = '一共有'.count($image_list).'个图形文件。当前背景：'.$image.'<br><br>';
+$fl = '一共有'.count($image_list).'个图形文件。当前背景文件：<font color=#A000FF><b>'.$image.'</b></font><br><br>';
 $n = 1;
 foreach($image_list as $v){
     $fn = substr($v,10);
-    if ($fn==$image) $fn = '<b><font size="+2em">'.$fn.'</font></b>';
-    $fl = $fl.'<a class="sxyhButton_img" href="'.$v.'"><img width="300" border="0" src=../images/'.$v.'>'.$n.' - [ '.$fn.' ]</a>'.PHP_EOL;
+    $bl = ($fn==$image)?'border="8" style="border-style:double;"':'border="0"';
+    $al = ($fn==$image)?'style="color:#A000FF;font-weight:"':'';
+    $fl = $fl.'<a class="sxyhButton_img" '.$al.' href="showimage.php?file='.$fn.'"><img width="280" '.$bl.' src=../images/'.$v.'>'.$n.' - [ '.$fn.' ]</a>'.PHP_EOL;
     $n++;
 }
 
 $fl = $fl.'<br><br><a href="../">返回导航</a>';
-$fl = $fl.'<style> .sxyhButton_img {background-color:transparent;border-radius:0px;border:0px solid  #transparent;display:inline-block;cursor:pointer;font-family:宋体;font-size:12px;text-decoration:none;margin:10px;width:300px; height:28px;line-height:28px;}</style>';
+$fl = $fl.'<style> .sxyhButton_img {background-color:transparent;border-radius:0px;border:0px solid  #transparent;display:inline-block;cursor:pointer;font-family:宋体;font-size:12px;text-decoration:none;margin:10px;width:280px; height:28px;line-height:28px;}</style>';
 
 echo $fl;
 
