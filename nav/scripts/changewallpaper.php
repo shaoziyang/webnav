d525dc63b94c9927c5690d6cc28d83fa0d70a37c
shaoziyang <shaoziyang@126.com>
2023-11-22 16:22:21 +0800

    Update changewallpaper.php

--- nav/scripts/changewallpaper.php
+++ nav/scripts/changewallpaper.php
@@ -2,5 +2,5 @@
 var xhr = new XMLHttpRequest();
 xhr.open('GET', 'scripts/wallpaper.php?force=1', true);
 xhr.send(null);
-setTimeout(function(){ history.back();}, 1);
+setTimeout(function(){ history.back();}, 50);
 </script>
