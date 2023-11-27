71c217fb4eb19fdbbfe4c02663b2544842399773
shaoziyang <shaoziyang@126.com>
2023-11-27 23:09:12 +0800

    图片加相框

--- nav/scripts/showimagelist.php
+++ nav/scripts/showimagelist.php
@@ -65,9 +65,9 @@ $fl = '一共有'.count($image_list).'个图形文件。当前背景文件：<fo
 $n = 1;
 foreach($image_list as $v){
     $fn = substr($v,10);
-    $bl = ($fn==$image)?'border="8" style="border-style:double;"':'border="0"';
+    $bl = ($fn==$image)?'border="8" style="border-style:double;"':'border="8" style="border-color:rgb(230,158,41); border-style:ridge;"';
     $al = ($fn==$image)?'style="color:#A000FF;font-weight:"':'';
-    $fl = $fl.'<a class="sxyhButton_img" '.$al.' href="showimage.php?file='.$fn.'">'.$n.' - [ '.$fn.' ]<img width="280" '.$bl.' src=../images/'.$v.'></a>'.PHP_EOL;
+    $fl = $fl.'<a class="sxyhButton_img" '.$al.' href="showimage.php?file='.$fn.'"><img width="280" '.$bl.' src=../images/'.$v.'>'.$n.' - [ '.$fn.' ]</a>'.PHP_EOL;
     $n++;
 }
 
