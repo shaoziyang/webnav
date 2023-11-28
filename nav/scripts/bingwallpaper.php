44051845238220f57c0f28d1da4c8070440ca5b8
shaoziyang <shaoziyang@126.com>
2023-11-28 11:05:14 +0800

    调整图像左对齐

--- nav/scripts/bingwallpaper.php
+++ nav/scripts/bingwallpaper.php
@@ -4,6 +4,12 @@
 <link type="text/css" rel="stylesheet" href="../styles/styles-custom.css" media="screen,projection"/>
 <link type="text/css" rel="stylesheet" href="../styles/styles.css" media="screen,projection"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<style>
+   .sxyhImg_01 {
+      text-align: left;
+      display:inline-block;
+   }
+</style>
 </head>
 <body>
 
@@ -17,12 +23,12 @@ if (preg_match("/<url>(.+?)<\/url>/", $str, $matches)) {
 }
 
 if ($imgurl) {
-    echo '<H1>必应每日一图</H1>';
+    echo '<H1>必应每日一图</H1><br>';
     $v1 = parse_url($imgurl);
     parse_str($v1['query'], $v2);
     $fn=$v2['id'];
 
-    echo '<div><a href="'.$imgurl.'"><img width="800" src="'.$imgurl.'"></a><div>';
+    echo '<div  class="sxyhImg_01"><a href="'.$imgurl.'"><img width="800" src="'.$imgurl.'"></a></div>';
     echo '<br><br><br>获取时间：'.date("Y-m-d H:i:s").'<br>图像日期：'.$dt.'<br>图像说明：'.$cr.'<br><br>图像链接：<a href="'.$imgurl.'">'.$imgurl.'</a><br><br>';
     
 } else {
