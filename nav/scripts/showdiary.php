760e194fe3b64532f5023cdb2a3dc38c909ab814
shaoziyang <shaoziyang@126.com>
2023-12-19 00:20:35 +0800

    移动 diary 数据文件
    为方便维护，将diary数据文件移至  datasets/diary 目录

--- nav/scripts/showdiary.php
+++ nav/scripts/showdiary.php
@@ -1,13 +1,17 @@
 <?php
 
+$base_path = dirname(__DIR__);
+$diary = $base_path.'/datasets/diary/diary.txt';
+$diary_tmp = $base_path.'/datasets/diary/diary.tmp';
+
 $mode = 'day';
 if(isset($_GET['mode']))
 	$mode = $_GET['mode'];
 
-if (!file_exists('diary.txt'))
+if (!file_exists($diary))
 	exit('学而时习之，不亦说乎');
 
-$lines = file('diary.txt', FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
+$lines = file($diary, FILE_SKIP_EMPTY_LINES|FILE_IGNORE_NEW_LINES);
 $nl = count($lines);
 
 if ($nl == 0)
@@ -17,8 +21,8 @@ if ($mode == 'day') {
 	$day = -1;
 	$n = rand(0, $nl);
 	$new = true;
-	if (file_exists('diary.tmp')) {
-		$tmp = file('diary.tmp');
+	if (file_exists($diary_tmp)) {
+		$tmp = file($diary_tmp);
 		if (count($tmp) > 1) {
 			try {
 				$day = $tmp[0];
@@ -34,7 +38,7 @@ if ($mode == 'day') {
 			while ($x == $n)
 				$n = rand(0, $nl);
 		}
-		$fp = fopen('diary.tmp', "w+");
+		$fp = fopen($diary_tmp, "w+");
 		if ($fp) {
 			$canWrite = false;
 			while (!$canWrite)
